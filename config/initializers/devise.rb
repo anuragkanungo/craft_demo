5031e2c6652ee42606b3e05a766147f93c48ec0a
Anurag Kanungo <anuragkanungo@gmail.com>
2016-09-29 15:33:16 +0530

    Fixing LDAP. works now

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,14 +3,14 @@
 Devise.setup do |config|
   # ==> LDAP Configuration 
   # config.ldap_logger = true
-  config.ldap_create_user = false
+  config.ldap_create_user = true
   # config.ldap_update_password = true
   # config.ldap_config = "#{Rails.root}/config/ldap.yml"
   # config.ldap_check_group_membership = false
   # config.ldap_check_group_membership_without_admin = false
-  # config.ldap_check_attributes = false
-  # config.ldap_use_admin_to_bind = false
-  # config.ldap_ad_group_check = false
+  config.ldap_check_attributes = false
+  config.ldap_use_admin_to_bind = true
+  config.ldap_ad_group_check = true
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
